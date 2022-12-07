5ddf4970f4ac0e938a22a8083aa0123c2321641e
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-07 20:13:14 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,20 @@
+'''
+DATE:04TH NOV 2022
+DAY: SUNDAY
+TOPIC: TASK
+AUTHOR: RAMA BHARGAVI
+'''
+#sum of the numbers
+a=[3,9,5]
+print(a[0]+a[1]+a[2])#17
+print(sum(a))#17
+#to find how many letters
+q=[9,7,8,2,3]
+c=0
+for i in q:
+    c+=1
+    print(c) #1 2 3 4 5
+print(len(q))   #5
+#multiplication of tables
+for i in range(1,11):
+    print(2,'X',i,'=',2*i)#2 table
