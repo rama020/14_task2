0a818d7558f1d3d48903a555ca784f6e1935cf4b
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-07 20:40:14 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,17 @@
+'''
+DATE:04TH NOV 2022
+DAY: SUNDAY
+TOPIC: TASK
+AUTHOR: rama bhargavi
+'''
+#elements exists list or not
+n=[int(x) for x in input('enter some integers:').split()]#4 2 6 7 8
+f=int(input('which element you want to find? :'))#5
+for i in n:
+   if(f in n): 
+    print('the value',f, 'is found')
+    break
+else:
+    print('the value',f,'is not found')#the value 5 is not found
+ 
+    
